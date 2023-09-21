797f205e3a587375293efa3952feaa6a759edf34
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-09-21 03:07:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import './App.css';
-import MyGridLayout from './MyGridLayout';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Signin from './pages/Signin/Signin';
+import Signup from './pages/Signup/Signup';
+import Gallery from './pages/Gallery/Gallery';
 
 function App() {
-  return <MyGridLayout />;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/gallery' element={<Gallery />} />
+    </Routes>
+  );
 }
 
 export default App;
